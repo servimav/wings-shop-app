050a1be64eaa352a31b4f4a4e36a6a6fd5559f7b
AdriCQ <adrian.capote95@zoho.com>
2022-07-16 10:43:06 -0400

    Vendor Contact

--- src/vendors/pages/VendorHelp.vue
+++ src/vendors/pages/VendorHelp.vue
@@ -1,19 +1,19 @@
 <template>
   <q-page padding class="q-gutter-y-sm">
-    <title-widget :data="{ title: 'Ayuda' }" />
-
     <q-card>
-      <q-card-section>
-        <div class="text-h6">Our Changing Planet</div>
-        <div class="text-subtitle2">by John Doe</div>
+      <q-card-section class="text-center">
+        <div class="text-h6">Envíenos su pregunta</div>
+        <div class="text-subtitle2">
+          Nuestro deber es ayudarlo a mejorar sus ventas con nuestros servicios
+        </div>
       </q-card-section>
       <q-card-section>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        <contact-form />
       </q-card-section>
     </q-card>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import TitleWidget from 'src/components/widgets/TitleWidget.vue';
+import ContactForm from 'src/components/forms/ContactForm.vue';
 </script>
