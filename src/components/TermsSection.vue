6951de6ef148b2d4be57e06b4517706fdb9e7612
AdriCQ <adrian.capote95@zoho.com>
2022-07-16 10:40:50 -0400

    Terms And Conditions

--- src/components/TermsSection.vue
+++ src/components/TermsSection.vue
@@ -4,11 +4,38 @@
       Términos y Condiciones de Venta
     </div>
 
-    <p class="text-justify" v-for="i in 10" :key="i">
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum repellendus,
-      tenetur aperiam optio ducimus repudiandae voluptates! Necessitatibus qui
-      hic numquam blanditiis atque placeat sapiente exercitationem odit,
-      molestias nisi fugiat possimus.
+    <p>
+      <b>Wings Shop</b> brinda la posibilidad a los <b>Vendedores</b> que posean
+      un negocio autorizado por las instituciones pertinentes publicar sus
+      <b>ofertas</b> en la plataforma para tener mayores ventas.
     </p>
+
+    <p>
+      Para ello <b>Wings Shop</b> se compromete a:
+      <ul>
+        <li>Garantizar la accesibilidad de las <b>ofertas</b> del <b>vendedor</b> a los <b>clientes</b></li>
+        <li>Mantener los servicios tecnológicos activos y resolver las interrupciones ocurridas</li>
+        <li>Asegurar los datos personales y los datos de las ventas del <b>vendedor</b> y el <b>cliente</b></li>
+      </ul>
+    </p>
+
+    <p>
+      Por otra parte el <b>Vendedor</b> tiene que:
+      <ul>
+        <li>Garantizar que sus datos son reales</li>
+        <li>Publicar <b>ofertas</b> acorde a sus actividades aprobadas por las instituciones pertinentes</li>
+        <li>Garantizar la calidad y disponibilidad de las ofertas que publique</li>
+        <li>Atender los pedidos de los clientes sin demoras intencionadas</li>
+        <li>Tratar a los clientes de forma cordial y con un lenguaje agradable</li>
+      </ul>
+    </p>
+
+    <p>En caso de no cumplir los requisitos <b>Wings Shop</b> puede cancelar temporal o definitivamente la cuenta del <b>Vendedor</b></p>
   </q-card-section>
 </template>
+
+<style scoped>
+p {
+  text-align: justify;
+}
+</style>
